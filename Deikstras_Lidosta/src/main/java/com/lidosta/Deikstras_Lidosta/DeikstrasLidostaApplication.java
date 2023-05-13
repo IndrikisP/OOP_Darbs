6b6294b23643eba53373d7309e927742cd9656fa
Aivars Orups <aivars.orups@lu.lv>
2023-05-13 17:18:21 +0300

    changed folder name

--- Deikstras_Lidosta/src/main/java/com/lidosta/Deikstras_Lidosta/DeikstrasLidostaApplication.java
+++ Deikstras_Lidosta/src/main/java/com/lidosta/Deikstras_Lidosta/DeikstrasLidostaApplication.java
@@ -1,4 +1,4 @@
-package com.example.Deikstras_Lidosta;
+package com.lidosta.Deikstras_Lidosta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
