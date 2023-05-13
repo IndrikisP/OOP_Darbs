9a3a7aee3105b748ca4e264e533cdcaedec8e47a
LAPTOP-DC932NPC\Indriķis <indrikispaiders@inbox.lv>
2023-05-13 17:01:47 +0300

    no message

---
+++ Deikstras_Lidosta/src/main/java/com/example/Deikstras_Lidosta/DeikstrasLidostaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Deikstras_Lidosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeikstrasLidostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeikstrasLidostaApplication.class, args);
+	}
+
+}
---
+++ Deikstras_Lidosta/src/test/java/com/example/Deikstras_Lidosta/DeikstrasLidostaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Deikstras_Lidosta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeikstrasLidostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
